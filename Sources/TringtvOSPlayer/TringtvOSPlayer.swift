78d545a7004898cb2ec13fbc4d472a5e12e6f104
Ravi Chandran <ravi.chandran@tringapps.com>
2024-08-12 19:04:23 +0530

    First Commit

--- Sources/TringtvOSPlayer/TringtvOSPlayer.swift
+++ Sources/TringtvOSPlayer/TringtvOSPlayer.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct DoSomeWork{
+    public static func DoSomeThing(){
+        print("Doing SomeThing")
+    }
+}
