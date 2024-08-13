28c989b3a43d9f5aa45934a493a81bf718364533
Ravi Chandran <ravi.chandran@tringapps.com>
2024-08-13 10:45:39 +0530

    0.0.2

---
+++ Sources/TringtvOSPlayer.xcframework/tvos-arm64/TringtvOSPlayer.framework/Headers/TringtvOSPlayer.h
@@ -0,0 +1,18 @@
+//
+//  TringtvOSPlayer.h
+//  TringtvOSPlayer
+//
+//  Created by Ravi Chandran on 11/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TringtvOSPlayer.
+FOUNDATION_EXPORT double TringtvOSPlayerVersionNumber;
+
+//! Project version string for TringtvOSPlayer.
+FOUNDATION_EXPORT const unsigned char TringtvOSPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TringtvOSPlayer/PublicHeader.h>
+
+
--- Sources/TringtvOSPlayer/TringtvOSPlayer.swift
+++
@@ -1,8 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-public struct DoSomeWork{
-    public static func DoSomeThing(){
-        print("Doing SomeThing")
-    }
-}
